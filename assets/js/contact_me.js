63ecd9d7cb4c2a2094c2c7a029ceec4ad2e7932c
Nikhil Gohil <nikhilgohil11@gmail.com>
2021-05-25 19:08:50 +0800

    alignment for subcribe

--- assets/js/contact_me.js
+++ assets/js/contact_me.js
@@ -52,7 +52,7 @@ $(function() {
           $('#success').html("<div class='alert alert-danger'>");
           $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
-          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
+          $('#success > .alert-danger').append($("<strong>").text("Hey " + firstName + ", sometimes form submission doesn't work! Report us on twitter: https://twitter.com/startuplimited"));
           $('#success > .alert-danger').append('</div>');
           //clear all fields
           $('#contactForm').trigger("reset");
